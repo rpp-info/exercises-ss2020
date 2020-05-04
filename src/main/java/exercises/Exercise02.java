4e0a38d6aaaaa705cc3e25ab6d73e50ec45eb59f
Stephan Seitz <stephan.seitz@fau.de>
2020-05-04 11:26:51 +0200

    Add skeleton for exercise 2

---
+++ src/main/java/exercises/Exercise02.java
@@ -0,0 +1,38 @@
+// <Your name> <your idm>
+// <your partners name> <partner idm>
+
+package exercises;
+
+import us.hebi.matlab.mat.format.Mat5;
+
+import java.io.IOException;
+import ij.gui.Plot;
+import java.io.File;
+
+public class Exercise02 {
+
+
+	public static void main(String[] args) throws IOException {
+		(new ij.ImageJ()).exitWhenQuitting(true);
+
+		System.out.println("Started with the following arguments");
+		for (String arg : args) {
+			System.out.println(arg);
+		}
+
+		if (args.length == 1) {
+			File file = new File(args[0]);
+			if (file.isFile()) {
+				// Your code here
+
+
+			} else {
+				System.err.println("Could not find " + file);
+			}
+
+		} else {
+			System.out.println("Wrong argcount: " + args.length);
+			System.exit(-1);
+		}
+	}
+}
