906c2f54825f6fa50564a1b2ab08b316e86ec815
Stephan Seitz <stephan.seitz@fau.de>
2020-05-12 08:22:19 +0200

    Add solution Exercise01

--- src/main/java/exercises/Exercise01.java
+++ src/main/java/exercises/Exercise01.java
@@ -1,11 +1,34 @@
-// <your name> <your idm>
-// <your partner's name> <your partner's idm> (if you submit with a group partner)
+
+/*
+ * MainClass.java
+ * Copyright (C) 2020 Stephan Seitz <stephan.seitz@fau.de>
+ *
+ * Distributed under terms of the GPLv3 license.
+ */
 package exercises;
 
+
+import mt.LinearFilter;
+import mt.Signal;
 
 public class Exercise01 {
 	public static void main(String[] args) {
 		(new ij.ImageJ()).exitWhenQuitting(true);
+
+		Signal signal = new mt.Signal(new float[]{0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, "f(x)");
+		signal.show();
+
+		LinearFilter filter1 = new mt.LinearFilter(new float[]{1.0f/3 ,1/3.f ,1/3.f}, "Filter 1");
+		Signal result1 = filter1.apply(signal);
+		result1.show();
+
+		LinearFilter filter2 = new mt.LinearFilter(new float[]{1/5.f, 1/5.f , 1/5.f, 1/5.f, 1/5.f}, "Filter 2");
+		Signal result2 = filter2.apply(signal);
+		result2.show();
+
+		LinearFilter filter3 = new mt.LinearFilter(new float[]{0.5f, 0.0f, -0.5f}, "Filter 3");
+		Signal result3 = filter3.apply(signal);
+		result3.show();
 
 
 	}
