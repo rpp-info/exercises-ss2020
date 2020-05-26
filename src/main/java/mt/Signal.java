ebd75cc44b22eee3e5f8dc02aa4fb05af2d35ffd
Stephan Seitz <stephan.seitz@fau.de>
2020-05-26 11:20:10 +0200

    Autoformat

--- src/main/java/mt/Signal.java
+++ src/main/java/mt/Signal.java
@@ -17,157 +17,157 @@ import java.util.stream.IntStream;
 
 public class Signal {
 
-	protected float[] buffer;
-	protected String name;
-	protected int minIndex = 0;
-	protected float spacing = 1.f;
+    protected float[] buffer;
+    protected String name;
+    protected int minIndex = 0;
+    protected float spacing = 1.f;
 
-	public Signal(int length) {
-		buffer = new float[length];
-		this.name = "unnamed";
-	}
+    public Signal(int length) {
+        buffer = new float[length];
+        this.name = "unnamed";
+    }
 
-	public Signal(int length, String name) {
-		buffer = new float[length];
-		this.name = name;
-	}
+    public Signal(int length, String name) {
+        buffer = new float[length];
+        this.name = name;
+    }
 
-	public Signal(float[] buffer, String name) {
-		this.buffer = buffer;
-		this.name = name;
-	}
+    public Signal(float[] buffer, String name) {
+        this.buffer = buffer;
+        this.name = name;
+    }
 
-	public void addNoise(float mean, float standardDeviation) {
-		Random rand = new Random();
-		for (int i = 0; i < buffer.length; i++) {
-			buffer[i] += mean + rand.nextGaussian() * standardDeviation;
-		}
-	}
+    public void addNoise(float mean, float standardDeviation) {
+        Random rand = new Random();
+        for (int i = 0; i < buffer.length; i++) {
+            buffer[i] += mean + rand.nextGaussian() * standardDeviation;
+        }
+    }
 
-	public void show(Plot plot) {
-		DisplayUtils.showArray(buffer, plot, minIndex, 1.0f);
-	}
+    public void show(Plot plot) {
+        DisplayUtils.showArray(buffer, plot, minIndex, 1.0f);
+    }
 
-	public void show() {
-		DisplayUtils.showArray(buffer, name, minIndex, 1.0f);
-	}
+    public void show() {
+        DisplayUtils.showArray(buffer, name, minIndex, 1.0f);
+    }
 
-	public int size() {
-		return buffer.length;
-	}
+    public int size() {
+        return buffer.length;
+    }
 
-	public float[] buffer() {
-		return buffer;
-	}
+    public float[] buffer() {
+        return buffer;
+    }
 
-	public void setBuffer(float[] buffer) {
-		this.buffer = buffer;
-	}
+    public void setBuffer(float[] buffer) {
+        this.buffer = buffer;
+    }
 
-	public int minIndex() {
-		return minIndex;
-	}
+    public int minIndex() {
+        return minIndex;
+    }
 
-	public int maxIndex() {
-		return minIndex + size() - 1;
-	}
+    public int maxIndex() {
+        return minIndex + size() - 1;
+    }
 
-	public float atIndex(int i) {
-		int arrayIdx = i - minIndex;
-		if (arrayIdx < 0 || arrayIdx >= buffer.length) {
-			return 0.f;
-		} else {
-			return buffer[arrayIdx];
-		}
-	}
+    public float atIndex(int i) {
+        int arrayIdx = i - minIndex;
+        if (arrayIdx < 0 || arrayIdx >= buffer.length) {
+            return 0.f;
+        } else {
+            return buffer[arrayIdx];
+        }
+    }
 
-	public void setAtIndex(int i, float value) {
-		int arrayIdx = i - minIndex;
-		buffer[arrayIdx] = value;
-	}
+    public void setAtIndex(int i, float value) {
+        int arrayIdx = i - minIndex;
+        buffer[arrayIdx] = value;
+    }
 
-	public String name() {
-		return name;
-	}
+    public String name() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-		public float sum() {
-		float rtn = 0;
-		for (float f : buffer) {
-			rtn += f;
-		}
-		return rtn;
-	}
+    public float sum() {
+        float rtn = 0;
+        for (float f : buffer) {
+            rtn += f;
+        }
+        return rtn;
+    }
 
-	public float mean() {
-		return sum() / size();
-	}
+    public float mean() {
+        return sum() / size();
+    }
 
-	public float variance() {
-		float rtn = 0.f;
-		for (float f : buffer) {
-			rtn += f * f;
-		}
-		rtn /= size();
-		var mean = mean();
-		rtn -= mean * mean;
-		return rtn;
-	}
+    public float variance() {
+        float rtn = 0.f;
+        for (float f : buffer) {
+            rtn += f * f;
+        }
+        rtn /= size();
+        var mean = mean();
+        rtn -= mean * mean;
+        return rtn;
+    }
 
-	public float stdDev() {
-		return (float) Math.sqrt(variance());
-	}
-		public float min() {
-		var min = Float.POSITIVE_INFINITY;
-		for (float f : buffer) {
-			min = Math.min(f, min);
-		}
-		return min;
-	}
+    public float stdDev() {
+        return (float) Math.sqrt(variance());
+    }
 
-	public float max() {
-		var max = Float.NEGATIVE_INFINITY;
-		for (float f : buffer) {
-			max = Math.max(f, max);
-		}
-		return max;
-	}
-    
+    public float min() {
+        var min = Float.POSITIVE_INFINITY;
+        for (float f : buffer) {
+            min = Math.min(f, min);
+        }
+        return min;
+    }
+
+    public float max() {
+        var max = Float.NEGATIVE_INFINITY;
+        for (float f : buffer) {
+            max = Math.max(f, max);
+        }
+        return max;
+    }
+
     public Signal minus(Signal other) {
-		if (size() != other.size()) {
-			throw new RuntimeException("Java sucks: both Signals don't have the same size!");
-		}
-		Signal result = new Signal(this.size(), "(" + name() + ") minus (" + other.name() + ")");
-		for (int i = 0; i < result.size(); i++) {
-			result.buffer()[i] = buffer()[i] - other.buffer()[i];
-		}
-		return result;
-	}
+        if (size() != other.size()) {
+            throw new RuntimeException("Java sucks: both Signals don't have the same size!");
+        }
+        Signal result = new Signal(this.size(), "(" + name() + ") minus (" + other.name() + ")");
+        for (int i = 0; i < result.size(); i++) {
+            result.buffer()[i] = buffer()[i] - other.buffer()[i];
+        }
+        return result;
+    }
 
-	public Signal plus(Signal other) {
-		if (size() != other.size()) {
-			throw new RuntimeException("Java sucks: both Signals don't have the same size!");
-		}
-		Signal result = new Signal(this.size(), "(" + name() + ") plus (" + other.name() + ")");
-		for (int i = 0; i < result.size(); i++) {
-			result.buffer()[i] = buffer()[i] + other.buffer()[i];
-		}
-		return result;
-	}
+    public Signal plus(Signal other) {
+        if (size() != other.size()) {
+            throw new RuntimeException("Java sucks: both Signals don't have the same size!");
+        }
+        Signal result = new Signal(this.size(), "(" + name() + ") plus (" + other.name() + ")");
+        for (int i = 0; i < result.size(); i++) {
+            result.buffer()[i] = buffer()[i] + other.buffer()[i];
+        }
+        return result;
+    }
 
+    public void fill(Function<Integer, Float> fillFunction) {
+        IntStream.range(minIndex(), maxIndex()).forEach(i -> setAtIndex(i, fillFunction.apply(i)));
+    }
 
-	public void fill(Function<Integer, Float> fillFunction) {
-		IntStream.range(minIndex(), maxIndex()).forEach(i -> setAtIndex(i, fillFunction.apply(i)));
-	}
+    public void setSpacing(float spacing) {
+        this.spacing = spacing;
+    }
 
-	public void setSpacing(float spacing) {
-	    this.spacing = spacing;
-	}
-
-	public float spacing() {
-	    return spacing;
-	}
+    public float spacing() {
+        return spacing;
+    }
 }
